87010718900e884bc43d4f34de9566ef1c8302d9
Yousef Elbaz <youseffelbazz.1@gmail.com>
2026-05-03 15:25:02 +0300

    jj

--- backend/ExpenseTracker.API/Program.cs
+++ backend/ExpenseTracker.API/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +15,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Add service layer (Dependency Inversion Principle)
 builder.Services.AddScoped<IExpenseService, ExpenseService>();
 
-// Add CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular",
